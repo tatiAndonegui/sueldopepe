d329b3f063976488ad5e9b5af179afd35f532f55
Tati <115169557+tatiAndonegui@users.noreply.github.com>
2022-11-07 12:26:41 -0300

    feat: add employee interface

---
+++ src/IEmpleado.java
@@ -0,0 +1,7 @@
+public interface IEmpleado {
+
+    double calcularSueldoNeto();
+    double calcularBonoPorPresentismo (int diasAusente);
+    double calcularBonoPorResultado();
+    double calcularSueldoTotal(int diasAusente);
+}
